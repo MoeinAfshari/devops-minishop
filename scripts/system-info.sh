23a223da67fe2a26ddd3f1a16c88170b1731859f
MoeinAfshari <afsharimoein79@gmail.com>
2026-07-12 11:35:11 +0330

    feat: add script header

--- scripts/system-info.sh
+++ scripts/system-info.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Display basic system information
+# ==============================
+# Script: system-info.sh
+# Author: Moein Afshari
+# Description: Displays basic system information
+# ==============================
 
-printf "Current User: %s\n" $(whoami)
-printf "Current Directory: %s\n" $(pwd)
-printf "Date & Time: %s %s\n" $(date +'%Y-%m-%d %H:%M')
-printf "Host Name: %s\n" $(hostname)
-printf "Kernel Version: %s\n" $(uname -r)
-echo -e "System Uptime: $(uptime)"
-printf "Number of CPUs: %s\n" $(nproc)
-printf "RAM Space(GB): %s\n" $(free -g | grep Mem: | awk '{print $2}')
-printf "Disk Empty Space(GB): %s\n" $(df -h --total | grep total | awk '{print $4}')
-printf "IP Address: %s\n" $(hostname -I | awk '{print $1}')
+printf "Current User: %s\n" "$USER"
+printf "Current Directory: %s\n" "$(pwd)"
+printf "Date & Time: %s\n" "$(date +'%Y-%m-%d %H:%M')"
+printf "Host Name: %s\n" "$(hostname)"
+printf "Kernel Version: %s\n" "$(uname -r)"
+printf "System Uptime: %s\n" "$(uptime -p)"
+printf "Number of CPUs: %s\n" "$(nproc)"
+printf "RAM Space(GB): %s\n" "$(free -h | awk '/Mem:/ {print $2}')"
+printf "Disk Empty Space(GB): %s\n" "$(df -h / | awk 'NR==2 {print $4}')"
+printf "IP Address: %s\n" "$(hostname -I | awk '{print $1}')"
